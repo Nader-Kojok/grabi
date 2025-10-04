f8dac77fcf6c31e0ec525f83b1a4fd158c04bbd8
Nader-Kojok <naderkojok@gmail.com>
2025-10-04 11:11:18 +0000

    chore: remove unused SQL migration and debug files

--- debug-env.js
+++
@@ -1,5 +0,0 @@
-// Debug script to check environment variables in the browser
-console.log('Environment variables check:');
-console.log('VITE_SUPABASE_URL:', import.meta.env.VITE_SUPABASE_URL);
-console.log('VITE_SUPABASE_ANON_KEY:', import.meta.env.VITE_SUPABASE_ANON_KEY ? 'SET' : 'NOT SET');
-console.log('All env vars:', import.meta.env);
